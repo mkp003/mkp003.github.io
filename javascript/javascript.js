ab8434be018d47e3e91f50927263e67963464352
mkp003 <mkp003@mail.usask.ca>
2018-01-08 22:49:06 -0600

    Added hover images for the projects.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,16 @@
 window.onload = function() {
+	
+	// Navbar items
 	var home = document.getElementById("HomeBtn");
 	var about = document.getElementById("AboutBtn");
 	var projects = document.getElementById("ProjectsBtn");
 	var education = document.getElementById("EducationBtn");
 	var contact = document.getElementById("ContactBtn");
+	
+	// Project images
+	var becauseItsThere = document.getElementById("BecauseItsThere");
+	var vrad = document.getElementById("V-Rad");
+	var gui = document.getElementById("ResearchVR");
 	
 	// Navbar hover-over Functions
 	home.onmouseover = function() {
@@ -46,5 +53,31 @@ window.onload = function() {
 	
 	contact.onmouseout = function() {
 		contact.className = "navbarItem";
+	}
+	
+	
+	// Image Hover in and outs for projects
+	becauseItsThere.onmouseover = function() {
+		becauseItsThere.src = "images/BecauseItsThereImageHover.jpg";
+	}
+	
+	becauseItsThere.onmouseout = function() {
+		becauseItsThere.src = "images/BecauseItsThereImage.jpg";
+	}
+	
+	vrad.onmouseover = function() {
+		vrad.src = "images/371project1Hover.jpg";
+	}
+	
+	vrad.onmouseout = function() {
+		vrad.src = "images/371project1.jpg";
+	}
+	
+	gui.onmouseover = function() {
+		gui.src = "images/481project1Hover.jpg";
+	}
+	
+	gui.onmouseout = function() {
+		gui.src = "images/481project1.jpg";
 	}
 }
