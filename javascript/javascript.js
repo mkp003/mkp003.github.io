251b51054a6e61da20704ae6a38a1c0c2e914fff
mkp003 <mkp003@mail.usask.ca>
2018-01-17 19:55:23 -0600

    Combined the two javascript files together. Having two window.onload functions caused issues with mouseover actions.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -80,4 +80,20 @@ window.onload = function() {
 	gui.onmouseout = function() {
 		gui.src = "images/481project1.JPG";
 	}
+	
+	// Grabbing the navbar 
+	var nav = document.getElementById("navbar");
+	// Grab the current position of the screen
+	var sticky = nav.offsetTop;
+
+	window.onscroll = function(){scrollNavbar()};
+	
+	// The check the position of the navbar to see if it needs to stay fixed
+	function scrollNavbar() {
+		if (window.pageYOffset >= sticky) {
+			nav.classList.add("stickyNavbar");
+		} else {
+			nav.classList.remove("stickyNavbar");
+		}
+	}
 }
